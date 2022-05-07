83d7259f843e699d2150886731003a861c9bbb5b
John D'Oriocourt <kibner@gmail.com>
2022-05-07 06:58:40 -0500

    - added some plain ui form options - updated main.js to hook into document ready state to do the things when necessary

---
+++ js/main.js
@@ -0,0 +1,16 @@
+(function () {
+  // readystatechange as event listener to insert or modify the DOM before DOMContentLoaded
+  document.addEventListener('readystatechange', event => {
+    if (event.target.readyState === 'interactive') {
+      _initLoader();
+    } else if (event.target.readyState === 'complete') {
+      _initApp();
+    }
+  });
+
+  function _initLoader() {
+  }
+
+  function _initApp() {
+  }
+}());
