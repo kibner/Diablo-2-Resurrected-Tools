dc6dec94507a4a4f94c95f93f52fa8675cf99923
John D'Oriocourt <kibner@gmail.com>
2022-05-07 07:38:23 -0500

    added code to get selected search parameters and send them to a skeleton search function

--- js/main.js
+++ js/main.js
@@ -9,8 +9,32 @@
   });
 
   function _initLoader() {
+    document.getElementById('runeword-form').addEventListener('input', _handleFormInputChange);
   }
 
   function _initApp() {
+  }
+
+  function _handleFormInputChange(event) {
+    if (event.target.tagName !== 'INPUT') {
+      return;
+    }
+
+    const sockets = Array.from(document.querySelectorAll('input[name="sockets"]:checked'))
+      .reduce((previousValue, currentValue) => previousValue.concat(currentValue.value), []);
+
+    const equipment = Array.from(document.querySelectorAll('input[name="equipment"]:checked'))
+      .reduce((previousValue, currentValue) => previousValue.concat(currentValue.value), []);
+
+    const searchParams = {
+      sockets: sockets,
+      equipment: equipment
+    };
+
+    _search(searchParams);
+  }
+
+  function _search(searchParams) {
+    console.log(searchParams);
   }
 }());
