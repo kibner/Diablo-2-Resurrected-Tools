75c7c44815035b1124cf05b146b7b866621fd954
John D'Oriocourt <kibner@gmail.com>
2022-05-07 08:54:37 -0500

    - removed 1 socket choice as those runewords don't exist - added ability for search to check for number of sockets - added ability to output search results as html

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 (function () {
   let _runewordForm;
+  const _runewords = _getAllRunewords();
 
   // readystatechange as event listener to insert or modify the DOM before DOMContentLoaded
   document.addEventListener('readystatechange', event => {
@@ -24,7 +25,7 @@
     }
 
     const sockets = Array.from(_runewordForm.querySelectorAll('input[name="sockets"]:checked'))
-      .reduce((previousValue, currentValue) => previousValue.concat(currentValue.value), []);
+      .reduce((previousValue, currentValue) => previousValue.concat(parseInt(currentValue.value)), []);
 
     const equipment = Array.from(_runewordForm.querySelectorAll('input[name="equipment"]:checked'))
       .reduce((previousValue, currentValue) => previousValue.concat(currentValue.value), []);
@@ -35,12 +36,61 @@
     };
 
     const searchResults = _search(searchParams);
+    const searchResultsHtml = _getSearchResultsHtml(searchResults);
+
     const resultElement = _runewordForm.querySelector('output[name=runeword-result]');
-    resultElement.innerHTML = searchResults;
+    resultElement.innerHTML = searchResultsHtml;
   }
 
   function _search(searchParams) {
-    console.log(searchParams);
-    return '<div style="color: red">successful search</div>';
+    return _runewords.runewords.reduce((previousValue, currentValue) => {
+      if (searchParams.sockets.includes(currentValue.runes.length)) {
+        return previousValue.concat(currentValue);
+      }
+
+      return previousValue;
+    }, []);
+  }
+
+  function _getSearchResultsHtml(searchResults) {
+    let html = ''
+
+    if (searchResults && searchResults.length > 0) {
+      const tableStartHtml = '<table><thead><tr><th>Runeword</th><th>Stats</th></tr></thead><tbody>';
+      const tableEndHtml = '</tbody></table>'
+
+      const resultsHtml = searchResults.reduce((previousValue, currentValue) => {
+        const name = `<div>${currentValue.name}</div>`;
+        const runes = `<div>${currentValue.runes.toString()}</div>`;
+        const character_level = `<div>${currentValue.character_level}</div>`;
+        const stats = currentValue.stats.reduce((previousStat, currentStat) => previousStat.concat(`<div>${currentStat}</div>`), '');
+
+        const rowStartHtml = '<tr>';
+        const rowEndHtml = '</tr>';
+
+        return previousValue.concat(rowStartHtml,
+          `<td>${String.prototype.concat(name, runes, character_level)}</td>`,
+          `<td>${stats}</td>`,
+          rowEndHtml);
+      }, '');
+
+      html = html.concat(tableStartHtml, resultsHtml, tableEndHtml);
+    }
+
+    return html;
+  }
+
+  function _getAllRunewords() {
+    return {
+      runewords: [
+        {
+          name: 'Steel',
+          runes: ['Tir', 'El'],
+          equipment: ['sword', 'axe', 'mace'],
+          character_level: 13,
+          stats: ['+25% Increased Attack Speed', '+20% Enhanced Damage', '+3 to Minimum Damage', '+3 to Maximum Damage', '+50 to Attack Rating', '50% Chance of Open Wounds', '+2 to Mana after each Kill', '+1 to Light Radius']
+        }
+      ]
+    };
   }
 }());
