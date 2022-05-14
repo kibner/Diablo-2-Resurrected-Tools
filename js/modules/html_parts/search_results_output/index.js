2b521433f363f74e6c958aab368ed985d8eae261
John D'Oriocourt <kibner@gmail.com>
2022-05-14 13:12:09 -0500

    refactored search results html generator to split out responsibilities

--- js/modules/html_parts/search_results_output/index.js
+++ js/modules/html_parts/search_results_output/index.js
@@ -31,47 +31,67 @@
     if (searchResults && searchResults.length > 0) {
       const tableStartHtml = '<table><thead><tr><th>Runeword</th><th>Stats</th></tr></thead><tbody>';
       const tableEndHtml = '</tbody></table>'
-
-      const resultsHtml = searchResults.reduce((previousValue, currentValue) => {
-        const name = `<div>${currentValue.name}</div>`;
-
-        const runes = `<div>${currentValue.runes.reduce((previousRunes, currentRune, currentIndex, array) => {
-          const rune = rune_data.find(value => value.id === currentRune);
-
-          if (rune) {
-            const separator = currentIndex < array.length - 1 ? `<span>, </span>` : '';
-
-            return previousRunes.concat(`<span>${rune.name}</span>`, separator);
-          } else {
-            return previousRunes;
-          }
-        }, '')}</div>`;
-
-        const equipment = `<div>${currentValue.equipment.reduce((previousEquipment, currentEquipment, currentIndex, array) => {
-          const equipment = equipment_data.find(value => value.id === currentEquipment);
-
-          if (equipment) {
-            const separator = currentIndex < array.length - 1 ? `<span>, </span>` : '';
-
-            return previousEquipment.concat(`<span>${equipment.name}</span>`, separator);
-          } else {
-            return previousEquipment;
-          }
-        }, '')}</div>`;
-
-        const character_level = `<div><span>Required level: </span><span>${currentValue.character_level}</span></div>`;
-        const stats = currentValue.stats.reduce((previousStats, currentStat) => previousStats.concat(`<div>${currentStat}</div>`), '');
-
-        return previousValue.concat('<tr>',
-          `<td>${String.prototype.concat(name, runes, equipment, character_level)}</td>`,
-          `<td>${stats}</td>`,
-          '</tr>');
-      }, '');
+      const resultsHtml = _getSearchResultsRowHtml(searchResults);
 
       html = html.concat(tableStartHtml, resultsHtml, tableEndHtml);
     }
 
     return html;
+  }
+
+  const _getSearchResultsRowHtml = function (searchResults) {
+    return searchResults.reduce((previousValue, currentValue) => {
+      const name = _getNameHtml(currentValue);
+      const runes = _getRunesHtml(currentValue);
+      const equipment = _getEquipmentHtml(currentValue);
+      const character_level = _getCharacterLevelHtml(currentValue);
+      const stats = _getStatsHtml(currentValue);
+
+      return previousValue.concat('<tr>',
+        `<td>${String.prototype.concat(name, runes, equipment, character_level)}</td>`,
+        `<td>${stats}</td>`,
+        '</tr>');
+    }, '');
+  }
+
+  const _getNameHtml = function (value) {
+    return `<div>${value.name}</div>`;
+  }
+
+  const _getRunesHtml = function (value) {
+    return `<div>${value.runes.reduce((previousRunes, currentRune, currentIndex, array) => {
+      const rune = rune_data.find(value => value.id === currentRune);
+
+      if (rune) {
+        const separator = currentIndex < array.length - 1 ? `<span>, </span>` : '';
+
+        return previousRunes.concat(`<span>${rune.name}</span>`, separator);
+      } else {
+        return previousRunes;
+      }
+    }, '')}</div>`;
+  }
+
+  const _getEquipmentHtml = function (value) {
+    return `<div>${value.equipment.reduce((previousEquipment, currentEquipment, currentIndex, array) => {
+      const equipment = equipment_data.find(value => value.id === currentEquipment);
+
+      if (equipment) {
+        const separator = currentIndex < array.length - 1 ? `<span>, </span>` : '';
+
+        return previousEquipment.concat(`<span>${equipment.name}</span>`, separator);
+      } else {
+        return previousEquipment;
+      }
+    }, '')}</div>`;
+  }
+
+  const _getCharacterLevelHtml = function (value) {
+    return `<div><span>Required level: </span><span>${value.character_level}</span></div>`;
+  }
+
+  const _getStatsHtml = function (value) {
+    return value.stats.reduce((previousStats, currentStat) => previousStats.concat(`<div>${currentStat}</div>`), '');
   }
 
   return {
