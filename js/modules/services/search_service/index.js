8a78cbf5d6c4a5170a7441c252714a8ef2b98cad
John D'Oriocourt <kibner@gmail.com>
2022-05-28 07:52:59 -0500

    - made equipment_data available to search - refactored search so each criterion has a function in order to make it more readable

--- js/modules/services/search_service/index.js
+++ js/modules/services/search_service/index.js
@@ -1,28 +1,45 @@
 (function (root, factory) {
   if (typeof define === 'function' && define.amd) {
     // AMD. Register as an anonymous module.
-    define(['../../data/runeword_data'], factory);
+    define([
+      '../../data/runeword_data',
+      '../../data/equipment_data'
+    ], factory);
   } else if (typeof module === 'object' && module.exports) {
     // Node. Does not work with strict CommonJS, but
     // only CommonJS-like environments that support module.exports,
     // like Node.
-    module.exports = factory(require('../../data/runeword_data'));
+    module.exports = factory(require(
+      '../../data/runeword_data',
+      '../../data/equipment_data'
+    ));
   } else {
     // Browser globals (root is window)
     root.search_service = factory(root.runeword_data);
   }
-}(typeof self !== 'undefined' ? self : this, function (runeword_data) {
+}(typeof self !== 'undefined' ? self : this, function (
+  runeword_data,
+  equipment_data
+) {
   // Just return a value to define the module export.
+  const _hasMatchingSocket = function (searchParamSockets, runeword) {
+    return searchParamSockets.sockets.length === 0 || searchParamSockets.sockets.includes(runeword.runes.length);
+  };
+
+  const _hasMatchingEquipment = function (searchParamEquipment, runeword) {
+    return searchParamEquipment.equipment.length === 0 || searchParamEquipment.equipment.some(equipment => runeword.equipment.includes(equipment));
+  };
+
   const _searchRunewords = function (searchParams) {
     return runeword_data.reduce((previousValue, currentValue) => {
-      if ((searchParams.sockets.length === 0 || searchParams.sockets.includes(currentValue.runes.length))
-        && (searchParams.equipment.length === 0 || searchParams.equipment.some(equipment => currentValue.equipment.includes(equipment)))) {
+      if (_hasMatchingSocket(searchParams.sockets, currentValue)
+        && _hasMatchingEquipment(searchParams.equipment, currentValue)) {
         return previousValue.concat(currentValue);
       }
 
       return previousValue;
     }, []);
-  }
+  };
 
   return {
     searchRunewords: _searchRunewords
