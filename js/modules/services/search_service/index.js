fcc9461d86500b3382f7f25ce1b556995f7e8caa
John D'Oriocourt <kibner@gmail.com>
2022-05-28 10:19:25 -0500

    fixed some issues with previous update

--- js/modules/services/search_service/index.js
+++ js/modules/services/search_service/index.js
@@ -15,7 +15,10 @@
     ));
   } else {
     // Browser globals (root is window)
-    root.search_service = factory(root.runeword_data);
+    root.search_service = factory(
+      root.runeword_data,
+      root.equipment_data
+    );
   }
 }(typeof self !== 'undefined' ? self : this, function (
   runeword_data,
@@ -23,11 +26,20 @@
 ) {
   // Just return a value to define the module export.
   const _hasMatchingSocket = function (searchParamSockets, runeword) {
-    return searchParamSockets.sockets.length === 0 || searchParamSockets.sockets.includes(runeword.runes.length);
+    return searchParamSockets.length === 0 || searchParamSockets.sockets.includes(runeword.runes.length);
   };
 
   const _hasMatchingEquipment = function (searchParamEquipment, runeword) {
-    return searchParamEquipment.equipment.length === 0 || searchParamEquipment.equipment.some(equipment => runeword.equipment.includes(equipment));
+    return searchParamEquipment.length === 0
+      || searchParamEquipment.some(equipment => {
+        // console.log({
+        //   runeword: runeword,
+        //   equipment: equipment_data[equipment]
+        // });
+
+        return runeword.equipment.includes(equipment);
+          // && runeword.runes.length <= equipment_data[equipment].max_sockets;
+      });
   };
 
   const _searchRunewords = function (searchParams) {
