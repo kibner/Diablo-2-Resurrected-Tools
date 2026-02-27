83f2495d702d39f25d160fc13c02b2650b4e2958
John D'Oriocourt <kibner@gmail.com>
2026-02-27 22:46:07 +0000

    added slot support for checkboxes for equipment fieldset

--- js/components/equipment-fieldset/index.js
+++ js/components/equipment-fieldset/index.js
@@ -1,4 +1,5 @@
 import templateHtmlText from 'bundle-text:./template.html';
+import { Equipment } from '../../data/equipment_data/index.js';
 
 class EquipmentFieldset extends HTMLElement {
   constructor() {
@@ -10,8 +11,27 @@ class EquipmentFieldset extends HTMLElement {
 
     const templateContent = template.content;
 
+    const fieldsetComponent =
+      templateContent.querySelector('fieldset-component');
+
+    this.appendCheckboxes(fieldsetComponent);
+
     const shadowRoot = this.attachShadow({ mode: 'open' });
     shadowRoot.appendChild(document.importNode(templateContent, true));
+  }
+
+  appendCheckboxes(fieldsetComponent) {
+    for (let i = 0; i < Equipment.length; i++) {
+      const checkboxComponent = document.createElement('checkbox-component');
+      checkboxComponent.slot = 'fields';
+
+      const checkboxName = document.createElement('span');
+      checkboxName.slot = 'checkbox-name';
+      checkboxName.innerHTML = Equipment[i].name;
+
+      checkboxComponent.appendChild(checkboxName);
+      fieldsetComponent.appendChild(checkboxComponent);
+    }
   }
 }
 
