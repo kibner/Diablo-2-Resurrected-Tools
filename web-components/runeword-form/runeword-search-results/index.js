c6f0e2036747ea0eead55edb1d751ed4ef94ee73
John D'Oriocourt <kibner@gmail.com>
2026-02-28 15:07:52 +0000

    began adding search results to web component

---
+++ web-components/runeword-form/runeword-search-results/index.js
@@ -0,0 +1,18 @@
+import templateHtmlText from 'bundle-text:./template.html';
+
+class RunewordSearchResults extends HTMLElement {
+  constructor() {
+    super();
+
+    const template = new DOMParser()
+      .parseFromString(templateHtmlText, 'text/html')
+      .querySelector('template');
+
+    const templateContent = template.content;
+
+    const shadowRoot = this.attachShadow({ mode: 'open' });
+    shadowRoot.appendChild(document.importNode(templateContent, true));
+  }
+}
+
+customElements.define('runeword-search-results', RunewordSearchResults);
