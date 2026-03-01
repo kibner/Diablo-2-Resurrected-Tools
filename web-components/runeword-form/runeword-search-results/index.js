a66f7112bfcfba4c85880f669f8238688067a596
John D'Oriocourt <kibner@gmail.com>
2026-03-01 05:05:42 +0000

    failed attempt at getting non-slot nodes added to runeword description

--- web-components/runeword-form/runeword-search-results/index.js
+++ web-components/runeword-form/runeword-search-results/index.js
@@ -1,5 +1,6 @@
 import templateHtmlText from 'bundle-text:./template.html';
 import { Runewords } from '../../../js/data/runeword_data/index.js';
+import { Runes } from '../../../js/data/rune_data/index.js';
 
 class RunewordSearchResults extends HTMLElement {
   constructor() {
@@ -35,6 +36,21 @@ class RunewordSearchResults extends HTMLElement {
       const requiredLevel = document.createElement('span');
       requiredLevel.slot = 'required-level';
       requiredLevel.innerHTML = runeword.character_level;
+
+      const runes = description.shadowRoot.querySelector('.runes');
+      console.log(description);
+
+      for (let j = 0; j < runeword.runes.length; j++) {
+        const rune = document.createElement('li');
+        rune.innerHTML = Runes.find(
+          (value) => value.id === runeword.runes[j],
+        ).name;
+
+        // console.log(runes);
+        runes.appendChild(document.importNode(rune, true));
+      }
+
+      const equipment = description.querySelector('.equipment');
 
       description.appendChild(runewordName);
       description.appendChild(requiredLevel);
